45e8bfd6fee3d78e732558936f0c0d9e67a7e886
Thiago Rios <thiago.mgoncalves@al.infnet.edu.br.com>
2024-04-02 22:13:19 -0300

    refatoração de códigos inutilizados

--- src/entidades/db_operations.py
+++ src/entidades/db_operations.py
@@ -1,14 +1,3 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
 
-db = SQLAlchemy()
-
-class OperationsDB():
-
-    def get_all_products(self, db):
-        query = text('SELECT * FROM produtos')
-        result = db.session.execute(query)
-        produtos = result.fetchall()
-        return produtos
-
-    
+db = SQLAlchemy()  
